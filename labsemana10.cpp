9effb28581d74e3a7b17f50f2e1f2886ade736c9
vicente-santander <127953756+vicente-santander@users.noreply.github.com>
2023-05-27 05:32:42 -0400

    Update labsemana10.cpp

--- labsemana10.cpp
+++ labsemana10.cpp
@@ -1,166 +1,121 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 using namespace std;
 
-struct Node {
-	int data;
-	Node* left;
-	Node* right;
+class grafo {
+	
+	public:
+		
+		void addEdge(vector<vector<int>>& graph, int u, int v) {
+			graph[u][v] = 1;
+			graph[v][u] = 1;
+		}
+
+		void printGraph(const vector<vector<int>>& graph) {
+			cout << "Adjacency Matrix:\n";
+			for (const auto& row : graph) {
+				for (int val : row) {
+					cout << val << " ";
+				}
+			cout << '\n';
+			}
+		}
+		
+		bool areAdjacent(const vector<vector<int>>& graph, int u, int v) {
+		    return graph[u][v] == 1 && graph[v][u] == 1;
+		}
+		
 };
 
-Node* createNode(int data) { // ejercicio 1
-	Node* newNode = new Node;
-	newNode->data = data;
-	newNode->left = NULL;
-	newNode->right = NULL;
-	return newNode;
-}
-
-Node* insert(Node* root, int data){ // ejercicio 2 
-	if(root == NULL){
-		root = createNode(data);
-	}
-	else{
-		if(root->data > data){
-			root->left = insert(root->left, data);
-		}
-		else{
-			root->right = insert(root->right, data);
-		}
-	}
-	return root;
-}
-
-void printTree_in_order(Node* root){ 
-	if (root == NULL) {
-		return;
-	}
-	printTree_in_order(root->left);
-	cout << root->data << " ";
-	printTree_in_order(root->right);
-}
-
-
-void printTree_pre_order(Node* root){ 
-	if (root == NULL) {
-		return;
-	}
-	cout << root->data << " ";
-	printTree_pre_order(root->left);
-	printTree_pre_order(root->right);
+class grafoDirigido {
+	
+	public:
 		
-}
+		void addEdge2(vector<vector<int>>& graph, int u, int v) {
+			graph[u][v] = 1;
+			
+		}
 
-void printTree_post_order(Node* root){ 
-	if (root == NULL) {
-		return;
-	}
-	printTree_post_order(root->left);
-	printTree_post_order(root->right);
-	cout << root->data << " ";
-}
+		void printGraph(const vector<vector<int>>& graph) {
+			cout << "Adjacency Matrix:\n";
+			for (const auto& row : graph) {
+				for (int val : row) {
+					cout << val << " ";
+				}
+			cout << '\n';
+			}
+		}
+		
+		bool areAdjacent(const vector<vector<int>>& graph, int u, int v) {
+		    return graph[u][v] == 1;
+		}
+		
+	
 
-void depthFirstSearch(Node* root) {
-    if (root == NULL) {
-        return;
-    }
-    cout << root->data << " ";
-    depthFirstSearch(root->left);
-    depthFirstSearch(root->right);
-}
 
-void breadthFirstSearch(Node* root) {
-    if (root == NULL) {
-        return;
-    }
-    
-    queue<Node*> q;
-    q.push(root);
-    
-    while (!q.empty()) {
-        Node* current = q.front();
-        q.pop();
-        
-        cout << current->data << " ";
-        
-        if (current->left != NULL) {
-            q.push(current->left);
-        }
-        
-        if (current->right != NULL) {
-            q.push(current->right);
-        }
-    }
-}
+};
+
 
 int main(){
-	Node* node1 = new Node;
-	node1->data = 1;
+	vector<vector<int>> graph(8, vector<int>(8, 0));
 	
-	Node* node2 = new Node;
-	node2->data = 2;
+	grafo graf;
+		
+	graf.addEdge(graph,0,1);
+	graf.addEdge(graph,0,2);
+	graf.addEdge(graph,1,3);
+	graf.addEdge(graph,1,4);
+	graf.addEdge(graph,1,5);
+	graf.addEdge(graph,2,3);
+	graf.addEdge(graph,2,5);
+	graf.addEdge(graph,2,6);
+	graf.addEdge(graph,3,4);
+	graf.addEdge(graph,4,5);
+	graf.addEdge(graph,4,7);
+	graf.addEdge(graph,5,6);
+	graf.addEdge(graph,5,7);
+	graf.addEdge(graph,6,7);
 	
-	Node* node3 = new Node;
-	node3->data = 3;
+	graf.printGraph(graph);
 	
-	Node* node4 = new Node;
-	node4->data = 4;
-	
-	Node* node5 = new Node;
-	node5->data = 5;
-	
-	Node* node6 = new Node;
-	node6->data = 6;
-	
-	Node* node7 = new Node;
-	node7->data = 7;
+	int vertex1 = 2;
+    int vertex2 = 3;
 
-	node1->left = node2;
-	node1->right = node3;
-	
-	node2->left = node4;
-	node2->right = node5;
-	
-	node3->left = node6;
-	node3->right = node7;
-	
-	node4->left = node4->right = node5->left = node5->right = node6->left = node6->right = node7->left = node7->right = NULL; 
-	
-	cout << "in-Order: ";
-	printTree_in_order(node1);
-	cout << endl;
-	cout << "pre-Order: ";
-	printTree_pre_order(node1);
-	cout << endl;
-	cout << "post-Order: ";
-	printTree_post_order(node1);
-	cout << endl;
-	
-	//ejercicio 5
-	
-	Node* root = NULL;	
-	int arreglo[10] ={21, 18, 6, 9, 10, 7, 19, 15, 12, 5};
-	
-	for (int i = 0; i < 10; i++) {
-        root = insert(root, arreglo[i]);
+    if (graf.areAdjacent(graph, vertex1, vertex2)) {
+        cout << "Los vertices " << vertex1 << " y " << vertex2 << " son adyacentes.\n";
+    } else {
+        cout << "Los vertices " << vertex1 << " y " << vertex2 << " no son adyacentes.\n";
     }
-	
-	cout << "ejercicio 5 ordenado: ";
-    printTree_in_order(root);
     cout << endl;
+    //---------------------------------------------------------------------------------------
     
-    //ejercicio 6
-	cout << "Depth First Search: ";
-    depthFirstSearch(root);
-    cout << endl;
+    vector<vector<int>> graph2(8, vector<int>(8, 0));
     
-    //ejercicio 7
-    cout << "Breadth-First Search: ";
-    breadthFirstSearch(root);
-    cout << endl;
+    grafoDirigido graf2;
 
-	
-	
+    graf2.addEdge2(graph2, 0, 1);
+    graf2.addEdge2(graph2, 0, 2);
+    graf2.addEdge2(graph2, 1, 3);
+    graf2.addEdge2(graph2, 1, 4);
+    graf2.addEdge2(graph2, 1, 5);
+    graf2.addEdge2(graph2, 2, 3);
+    graf2.addEdge2(graph2, 2, 5);
+    graf2.addEdge2(graph2, 2, 6);
+    graf2.addEdge2(graph2, 3, 4);
+    graf2.addEdge2(graph2, 4, 5);
+    graf2.addEdge2(graph2, 4, 7);
+    graf2.addEdge2(graph2, 5, 6);
+    graf2.addEdge2(graph2, 5, 7);
+    graf2.addEdge2(graph2, 6, 7);
+    
+    // Agregar una arista dirigida que crea un ciclo desde el vértice 7 al vértice 2
+    graf2.addEdge2(graph2, 6, 2);
+    
+    // Agregar una arista dirigida hacia el vértice 7 (que no puede ser alcanzado desde otros vértices)
+    graf2.addEdge2(graph2, 7, 7);
+    
+    graf2.printGraph(graph2);
+    
 	
 	return 0;
 }
